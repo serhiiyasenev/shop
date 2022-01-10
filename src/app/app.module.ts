75a32103489fc50a68c60cc348bdafebc2fc888d
serhiiyasenev <serhii.yasenev@gmail.com>
2022-01-10 12:28:32 +0200

    init (#1)
    Co-authored-by: Serhii Yasenev <serhii.yasenev@ihsmarkit.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './components/first/first.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [ AppComponent, FirstComponent, ProductComponent ],
+  imports: [ BrowserModule, AppRoutingModule ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
